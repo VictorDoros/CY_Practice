7ebc97457bb46990f52d22aa21358510179a3218
Victor Doros <dorosvictor@gmail.com>
2023-11-10 11:43:28 +0200

    Refactoring

--- cypress/e2e/signUp.js
+++ cypress/e2e/signUp.js
@@ -15,7 +15,6 @@ import data from "../fixtures/contactList_testData.json"
 import signUp from "../selectors/signUp.sel"
 import basicData from "../support/basic_data.json"
 
-
 //Call getBaseUrl() to get environment specific url value
 const url = new Utility().getBaseUrl()
 
@@ -26,247 +25,251 @@ const email = faker.internet.email()
 const invalidPassword = faker.internet.password({ length: 6 })
 const validPassword = faker.internet.password({ length: 7 })
 
-describe("Sign up page ", { tags: ["@webApp", "@functional", "@signUp"] }, () => {
-  beforeEach(() => {
-    stepDescription("Reach the page")
-    cy.visit(url)
-  })
+describe(
+  "Sign up page ",
+  { tags: ["@webApp", "@functional", "@signUp"] },
+  () => {
+    beforeEach(() => {
+      stepDescription("Reach the page")
+      cy.visit(url)
+    })
 
-  /**
-   * Basic test for Sign up
-   * #001
-   */
-  it("Check the 'Log In' URL", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #001
+     */
+    it("Check the 'Log In' URL", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check the 'Sign Up' URL")
-    checkURL(url + urls.signInPage)
-  })
+      stepDescription("Check the 'Sign Up' URL")
+      checkURL(url + urls.signInPage)
+    })
 
-  /**
-   * Basic test for Sign up
-   * #002
-   */
-  it("Cancel the 'Sign up' process", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #002
+     */
+    it("Cancel the 'Sign up' process", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check the 'Sign Up' URL")
-    checkURL(url + urls.signInPage)
+      stepDescription("Check the 'Sign Up' URL")
+      checkURL(url + urls.signInPage)
 
-    stepDescription("Click on [Cancel] button")
-    clickSimpleOnNthElement(signUp.cancelButton)
+      stepDescription("Click on [Cancel] button")
+      clickSimpleOnNthElement(signUp.cancelButton)
 
-    stepDescription(
-      "Check that the URL has changed (you shoukld be on 'Log in' page)"
-    )
-    checkURL(url + urls.loginPage)
-  })
+      stepDescription(
+        "Check that the URL has changed (you shoukld be on 'Log in' page)"
+      )
+      checkURL(url + urls.loginPage)
+    })
 
-  /**
-   * Basic test for Sign up
-   * #003
-   */
-  it("Submit the form with no data", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #003
+     */
+    it("Submit the form with no data", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check the 'Sign Up' URL")
-    checkURL(url + urls.signInPage)
+      stepDescription("Check the 'Sign Up' URL")
+      checkURL(url + urls.signInPage)
 
-    stepDescription("Click on [Cancel] button")
-    clickSimpleOnNthElement(signUp.cancelButton)
+      stepDescription("Click on [Cancel] button")
+      clickSimpleOnNthElement(signUp.cancelButton)
 
-    stepDescription(
-      "Check that the URL has changed (you shoukld be on 'Log in' page)"
-    )
-    checkURL(url + urls.loginPage)
-  })
+      stepDescription(
+        "Check that the URL has changed (you shoukld be on 'Log in' page)"
+      )
+      checkURL(url + urls.loginPage)
+    })
 
-  /**
-   * Basic test for Sign up
-   * #004
-   */
-  it("Click on [Submit] button with no filled in field", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #004
+     */
+    it("Click on [Submit] button with no filled in field", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check that no error message displayed")
-    checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
+      stepDescription("Check that no error message displayed")
+      checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription(
-      "Check the presence of the error when there is no filled in field"
-    )
-    checkElementStateAndHasText(
-      signUp.errorMessage,
-      basicData.stateData.beVisible,
-      data.signUp.noFilledInFieldErrorMessage
-    )
-  })
+      stepDescription(
+        "Check the presence of the error when there is no filled in field"
+      )
+      checkElementStateAndHasText(
+        signUp.errorMessage,
+        basicData.stateData.beVisible,
+        data.signUp.noFilledInFieldErrorMessage
+      )
+    })
 
-  /**
-   * Basic test for Sign up
-   * #005
-   */
-  it("Fill in the 'First name' field and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #005
+     */
+    it("Fill in the 'First name' field and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check that no error message displayed")
-    checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
+      stepDescription("Check that no error message displayed")
+      checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
 
-    stepDescription("Fill in the 'First name' field")
-    typeTextSimple(signUp.firstNameField, firstName)
+      stepDescription("Fill in the 'First name' field")
+      typeTextSimple(signUp.firstNameField, firstName)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription(
-      "Check the presence of the error when only the 'First name' field is filled in"
-    )
-    checkElementStateAndHasText(
-      signUp.errorMessage,
-      basicData.stateData.beVisible,
-      data.signUp.errorMessageWithoutFirstName
-    )
-  })
+      stepDescription(
+        "Check the presence of the error when only the 'First name' field is filled in"
+      )
+      checkElementStateAndHasText(
+        signUp.errorMessage,
+        basicData.stateData.beVisible,
+        data.signUp.errorMessageWithoutFirstName
+      )
+    })
 
-  /**
-   * Basic test for Sign
-   * #006
-   */
-  it("Fill in the 'First name' and 'Last name' fields and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign
+     * #006
+     */
+    it("Fill in the 'First name' and 'Last name' fields and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check that no error message displayed")
-    checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
+      stepDescription("Check that no error message displayed")
+      checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
 
-    stepDescription("Fill in the 'First name' field")
-    typeTextSimple(signUp.firstNameField, firstName)
+      stepDescription("Fill in the 'First name' field")
+      typeTextSimple(signUp.firstNameField, firstName)
 
-    stepDescription("Fill in the 'Last name' field")
-    typeTextSimple(signUp.lastNameField, lastName)
+      stepDescription("Fill in the 'Last name' field")
+      typeTextSimple(signUp.lastNameField, lastName)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription(
-      "Check the presence of the error when only the 'First name' field is filled in"
-    )
-    checkElementStateAndHasText(
-      signUp.errorMessage,
-      basicData.stateData.beVisible,
-      data.signUp.errorMessageWithoutFirstNameLastName
-    )
-  })
+      stepDescription(
+        "Check the presence of the error when only the 'First name' field is filled in"
+      )
+      checkElementStateAndHasText(
+        signUp.errorMessage,
+        basicData.stateData.beVisible,
+        data.signUp.errorMessageWithoutFirstNameLastName
+      )
+    })
 
-  /**
-   * Basic test for Sign up
-   * #007
-   */
-  it("Fill in the 'First name', 'Last name' and 'Email' fields and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #007
+     */
+    it("Fill in the 'First name', 'Last name' and 'Email' fields and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check that no error message displayed")
-    checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
+      stepDescription("Check that no error message displayed")
+      checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
 
-    stepDescription("Fill in the 'First name' field")
-    typeTextSimple(signUp.firstNameField, firstName)
+      stepDescription("Fill in the 'First name' field")
+      typeTextSimple(signUp.firstNameField, firstName)
 
-    stepDescription("Fill in the 'Last name' field")
-    typeTextSimple(signUp.lastNameField, lastName)
+      stepDescription("Fill in the 'Last name' field")
+      typeTextSimple(signUp.lastNameField, lastName)
 
-    stepDescription("Fill in the 'Email' field")
-    typeTextSimple(signUp.emailField, email)
+      stepDescription("Fill in the 'Email' field")
+      typeTextSimple(signUp.emailField, email)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription(
-      "Check the presence of the error when only the 'First name' field is filled in"
-    )
-    checkElementStateAndHasText(
-      signUp.errorMessage,
-      basicData.stateData.beVisible,
-      data.signUp.errorMessageWithoutFirstNameLastNameEmail
-    )
-  })
+      stepDescription(
+        "Check the presence of the error when only the 'First name' field is filled in"
+      )
+      checkElementStateAndHasText(
+        signUp.errorMessage,
+        basicData.stateData.beVisible,
+        data.signUp.errorMessageWithoutFirstNameLastNameEmail
+      )
+    })
 
-  /**
-   * Basic test for Sign up
-   * #008
-   */
-  it("Fill in all the fields with an invalid password and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #008
+     */
+    it("Fill in all the fields with an invalid password and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check that no error message displayed")
-    checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
+      stepDescription("Check that no error message displayed")
+      checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
 
-    stepDescription("Fill in the 'First name' field")
-    typeTextSimple(signUp.firstNameField, firstName)
+      stepDescription("Fill in the 'First name' field")
+      typeTextSimple(signUp.firstNameField, firstName)
 
-    stepDescription("Fill in the 'Last name' field")
-    typeTextSimple(signUp.lastNameField, lastName)
+      stepDescription("Fill in the 'Last name' field")
+      typeTextSimple(signUp.lastNameField, lastName)
 
-    stepDescription("Fill in the 'Email' field")
-    typeTextSimple(signUp.emailField, email)
+      stepDescription("Fill in the 'Email' field")
+      typeTextSimple(signUp.emailField, email)
 
-    stepDescription("Fill in the 'Password' field")
-    typeTextSimple(signUp.passwordField, invalidPassword)
+      stepDescription("Fill in the 'Password' field")
+      typeTextSimple(signUp.passwordField, invalidPassword)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription(
-      "Check the presence of the error when only the 'First name' field is filled in"
-    )
-    checkElementStateAndHasText(
-      signUp.errorMessage,
-      basicData.stateData.beVisible,
-      data.signUp.errorMessageInvalidPassowrd_firstPart +
-        invalidPassword +
-        data.signUp.errorMessageInvalidPassowrd_secondPart
-    )
-  })
+      stepDescription(
+        "Check the presence of the error when only the 'First name' field is filled in"
+      )
+      checkElementStateAndHasText(
+        signUp.errorMessage,
+        basicData.stateData.beVisible,
+        data.signUp.errorMessageInvalidPassowrd_firstPart +
+          invalidPassword +
+          data.signUp.errorMessageInvalidPassowrd_secondPart
+      )
+    })
 
-  /**
-   * Basic test for Sign up
-   * #009
-   */
-  it("Fill in all the fields with a valid password and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic test for Sign up
+     * #009
+     */
+    it("Fill in all the fields with a valid password and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Check that no error message displayed")
-    checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
+      stepDescription("Check that no error message displayed")
+      checkElementState(signUp.errorMessage, basicData.stateData.notBeVisible)
 
-    stepDescription("Fill in the 'First name' field")
-    typeTextSimple(signUp.firstNameField, firstName)
+      stepDescription("Fill in the 'First name' field")
+      typeTextSimple(signUp.firstNameField, firstName)
 
-    stepDescription("Fill in the 'Last name' field")
-    typeTextSimple(signUp.lastNameField, lastName)
+      stepDescription("Fill in the 'Last name' field")
+      typeTextSimple(signUp.lastNameField, lastName)
 
-    stepDescription("Fill in the 'Email' field")
-    typeTextSimple(signUp.emailField, email)
+      stepDescription("Fill in the 'Email' field")
+      typeTextSimple(signUp.emailField, email)
 
-    stepDescription("Fill in the 'Password' field")
-    typeTextSimple(signUp.passwordField, validPassword)
+      stepDescription("Fill in the 'Password' field")
+      typeTextSimple(signUp.passwordField, validPassword)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription("Check that the page was switched to 'Contact list'")
-    checkURL(url + urls.contactList)
-  })
+      stepDescription("Check that the page was switched to 'Contact list'")
+      checkURL(url + urls.contactList)
+    })
 
-  after(() => {
-    Cypress.session.clearAllSavedSessions()
-  })
-})
+    after(() => {
+      Cypress.session.clearAllSavedSessions()
+    })
+  }
+)
--- cypress/e2e/signUpScreenshotTests.js
+++ cypress/e2e/signUpScreenshotTests.js
@@ -15,148 +15,152 @@ import basicData from "../support/basic_data.json"
 //Call getBaseUrl() to get environment specific url value
 const url = new Utility().getBaseUrl()
 
-describe("Sign up - Screenshot tests ", { tags: ["@webApp", "@visual"] }, () => {
-  beforeEach(() => {
-    stepDescription("Reach the page")
-    cy.visit(url)
-  })
+describe(
+  "Sign up - Screenshot tests ",
+  { tags: ["@webApp", "@visual"] },
+  () => {
+    beforeEach(() => {
+      stepDescription("Reach the page")
+      cy.visit(url)
+    })
 
-  /**
-   * Basic screenshot test for Sign up
-   * #001
-   */
-  it("Click on [Submit] button with no filled in field", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic screenshot test for Sign up
+     * #001
+     */
+    it("Click on [Submit] button with no filled in field", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription("Wait until the error message is displayed")
-    waitUntilElementIsDisplayedOrHidden(
-      signUp.errorMessage,
-      basicData.stateData.beVisible
-    )
+      stepDescription("Wait until the error message is displayed")
+      waitUntilElementIsDisplayedOrHidden(
+        signUp.errorMessage,
+        basicData.stateData.beVisible
+      )
 
-    stepDescription("Take a screenshot with the displayed error")
-    compareSnapshot(data.screenshotTest.firstSignUpError)
-  })
+      stepDescription("Take a screenshot with the displayed error")
+      compareSnapshot(data.screenshotTest.firstSignUpError)
+    })
 
-  /**
-   * Basic screenshot test for Sign up
-   * #002
-   */
-  it("Fill in the 'First name' field and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic screenshot test for Sign up
+     * #002
+     */
+    it("Fill in the 'First name' field and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Fill in the 'First name' field e.g. 'Carol'")
-    typeTextSimple(signUp.firstNameField, data.signUp.firstName)
+      stepDescription("Fill in the 'First name' field e.g. 'Carol'")
+      typeTextSimple(signUp.firstNameField, data.signUp.firstName)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription("Wait until the error message is displayed")
-    waitUntilElementIsDisplayedOrHidden(
-      signUp.errorMessage,
-      basicData.stateData.beVisible
-    )
+      stepDescription("Wait until the error message is displayed")
+      waitUntilElementIsDisplayedOrHidden(
+        signUp.errorMessage,
+        basicData.stateData.beVisible
+      )
 
-    stepDescription("Take a screenshot with the displayed error")
-    compareSnapshot(data.screenshotTest.secondSignUpError)
-  })
+      stepDescription("Take a screenshot with the displayed error")
+      compareSnapshot(data.screenshotTest.secondSignUpError)
+    })
 
-  /**
-   * Basic screenshot test for Sign up
-   * #003
-   */
-  it("Fill in the 'First name' and 'Last name' fiels and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic screenshot test for Sign up
+     * #003
+     */
+    it("Fill in the 'First name' and 'Last name' fiels and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Fill in the 'First name' field")
-    typeTextSimple(signUp.firstNameField, data.signUp.firstName)
+      stepDescription("Fill in the 'First name' field")
+      typeTextSimple(signUp.firstNameField, data.signUp.firstName)
 
-    stepDescription("Fill in the 'Last name' field e.g. 'Marks'")
-    typeTextSimple(signUp.lastNameField, data.signUp.lastName)
+      stepDescription("Fill in the 'Last name' field e.g. 'Marks'")
+      typeTextSimple(signUp.lastNameField, data.signUp.lastName)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription("Wait until the error message is displayed")
-    waitUntilElementIsDisplayedOrHidden(
-      signUp.errorMessage,
-      basicData.stateData.beVisible
-    )
+      stepDescription("Wait until the error message is displayed")
+      waitUntilElementIsDisplayedOrHidden(
+        signUp.errorMessage,
+        basicData.stateData.beVisible
+      )
 
-    stepDescription("Take a screenshot with the displayed error")
-    compareSnapshot(data.screenshotTest.thirdSignUpError)
-  })
+      stepDescription("Take a screenshot with the displayed error")
+      compareSnapshot(data.screenshotTest.thirdSignUpError)
+    })
 
-  /**
-   * Basic screenshot test for Sign
-   * #004
-   */
-  it("Fill in the 'First name', 'Last name' and 'Email' fiels and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic screenshot test for Sign
+     * #004
+     */
+    it("Fill in the 'First name', 'Last name' and 'Email' fiels and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Fill in the 'First name' field e.g. 'Carol'")
-    typeTextSimple(signUp.firstNameField, data.signUp.firstName)
+      stepDescription("Fill in the 'First name' field e.g. 'Carol'")
+      typeTextSimple(signUp.firstNameField, data.signUp.firstName)
 
-    stepDescription("Fill in the 'Last name' field e.g. 'Marks'")
-    typeTextSimple(signUp.lastNameField, data.signUp.lastName)
+      stepDescription("Fill in the 'Last name' field e.g. 'Marks'")
+      typeTextSimple(signUp.lastNameField, data.signUp.lastName)
 
-    stepDescription("Fill in the 'Email' field e.g. 'carolmarks@email.com'")
-    typeTextSimple(signUp.emailField, data.signUp.email)
+      stepDescription("Fill in the 'Email' field e.g. 'carolmarks@email.com'")
+      typeTextSimple(signUp.emailField, data.signUp.email)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription("Wait until the error message is displayed")
-    waitUntilElementIsDisplayedOrHidden(
-      signUp.errorMessage,
-      basicData.stateData.beVisible
-    )
+      stepDescription("Wait until the error message is displayed")
+      waitUntilElementIsDisplayedOrHidden(
+        signUp.errorMessage,
+        basicData.stateData.beVisible
+      )
 
-    stepDescription("Take a screenshot with the displayed error")
-    compareSnapshot(data.screenshotTest.forthSignUpError)
-  })
+      stepDescription("Take a screenshot with the displayed error")
+      compareSnapshot(data.screenshotTest.forthSignUpError)
+    })
 
-  /**
-   * Basic screenshot test for Sign up
-   * #005
-   */
-  it("Fill in all the fiels with a invalid password and click on [Submit] button", () => {
-    stepDescription("Click on [Sign up] button")
-    clickSimpleOnNthElement(signUp.signInButton)
+    /**
+     * Basic screenshot test for Sign up
+     * #005
+     */
+    it("Fill in all the fiels with a invalid password and click on [Submit] button", () => {
+      stepDescription("Click on [Sign up] button")
+      clickSimpleOnNthElement(signUp.signInButton)
 
-    stepDescription("Fill in the 'First name' field e.g. 'Carol'")
-    typeTextSimple(signUp.firstNameField, data.signUp.firstName)
+      stepDescription("Fill in the 'First name' field e.g. 'Carol'")
+      typeTextSimple(signUp.firstNameField, data.signUp.firstName)
 
-    stepDescription("Fill in the 'Last name' field e.g. 'Marks'")
-    typeTextSimple(signUp.lastNameField, data.signUp.lastName)
+      stepDescription("Fill in the 'Last name' field e.g. 'Marks'")
+      typeTextSimple(signUp.lastNameField, data.signUp.lastName)
 
-    stepDescription("Fill in the 'Email' field e.g. 'carolmarks@email.com'")
-    typeTextSimple(signUp.emailField, data.signUp.email)
+      stepDescription("Fill in the 'Email' field e.g. 'carolmarks@email.com'")
+      typeTextSimple(signUp.emailField, data.signUp.email)
 
-    stepDescription("Fill in the 'Password' field e.g. '123456'")
-    typeTextSimple(signUp.passwordField, data.signUp.invalidPassword)
+      stepDescription("Fill in the 'Password' field e.g. '123456'")
+      typeTextSimple(signUp.passwordField, data.signUp.invalidPassword)
 
-    stepDescription("Click on [Submit] button")
-    clickSimpleOnNthElement(signUp.submitButton)
+      stepDescription("Click on [Submit] button")
+      clickSimpleOnNthElement(signUp.submitButton)
 
-    stepDescription("Wait until the error message is displayed")
-    waitUntilElementIsDisplayedOrHidden(
-      signUp.errorMessage,
-      basicData.stateData.beVisible
-    )
+      stepDescription("Wait until the error message is displayed")
+      waitUntilElementIsDisplayedOrHidden(
+        signUp.errorMessage,
+        basicData.stateData.beVisible
+      )
 
-    stepDescription("Take a screenshot with the displayed error")
-    compareSnapshot(data.screenshotTest.fifthSignUpError)
-  })
+      stepDescription("Take a screenshot with the displayed error")
+      compareSnapshot(data.screenshotTest.fifthSignUpError)
+    })
 
-  after(() => {
-    Cypress.session.clearAllSavedSessions()
-  })
-})
+    after(() => {
+      Cypress.session.clearAllSavedSessions()
+    })
+  }
+)
